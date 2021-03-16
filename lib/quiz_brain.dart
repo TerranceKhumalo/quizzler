fbc40e93d6e5c0fd21b3c96a67bc7cc471e68d73
TerranceKhumalo <khumaloterrance@gmail.com>
2021-03-16 15:28:50 +0200

    "Add OOP to quiz app."

--- lib/quiz_brain.dart
+++ lib/quiz_brain.dart
@@ -38,16 +38,23 @@ class QuizBrain {
   }
 
   String getQuestionText() {
-    return _questionBank[_questionNumber].questionText;
+    return this._questionBank[_questionNumber].questionText;
   }
 
   bool getCorrectAnswer() {
-    return _questionBank[_questionNumber].questionAnswer;
+    return this._questionBank[_questionNumber].questionAnswer;
   }
 
-  //TODO: Step 3 Part A - Create a method called isFinished() here that checks to see if we have reached the last question. It should return (have an output) true if we've reached the last question and it should return false if we're not there yet.
+  bool isFinished() {
+    if (_questionNumber >= _questionBank.length - 1) {
+      print('We reached the end isFinished.');
+      return true;
+    }
+    print('We have not reached the end.');
+    return false;
+  }
 
-  //TODO: Step 3 Part B - Use a print statement to check that isFinished is returning true when you are indeed at the end of the quiz and when a restart should happen.
-
-  //TODO: Step 4 Part B - Create a reset() method here that sets the questionNumber back to 0.
+  void resetQuesions() {
+    this._questionNumber = 0;
+  }
 }
